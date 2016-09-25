6a5e06153a41d0e4272f1198663605c581ca559c
Todor Iliev <todor@itprism.com>
2016-09-25 17:13:52 +0300

    version 1.2

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 Crowdfunding CRON - Updates Notifier
 ==========================
-( Version 1.1 )
+( Version 1.2 )
 - - -
 
 This is a CRON plug-in for CrowdFunding platform that sends notifications to users when a project owner add a update record.
 
 Changelog
 ---------
+
+###v1.2
+* It was made compatible with latest version of EmailTemplates.
+* Improved code quality.
--- updatesnotifier.php
+++ updatesnotifier.php
@@ -3,8 +3,8 @@
  * @package      Crowdfunding
  * @subpackage   Plug-ins
  * @author       Todor Iliev
- * @copyright    Copyright (C) 2015 Todor Iliev <todor@itprism.com>. All rights reserved.
- * @license      http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @copyright    Copyright (C) 2016 Todor Iliev <todor@itprism.com>. All rights reserved.
+ * @license      GNU General Public License version 3 or later; see LICENSE.txt
  */
 
 // no direct access
@@ -20,81 +20,78 @@ class plgCrowdfundingCronUpdatesNotifier extends JPlugin
 {
     public function onCronNotify($context)
     {
-        if (strcmp("com_crowdfunding.cron.notify.updates", $context) != 0) {
+        if (strcmp('com_crowdfunding.cron.notify.updates', $context) !== 0) {
             return;
         }
 
-        jimport("Prism.init");
-        jimport("Crowdfunding.init");
+        jimport('Prism.init');
+        jimport('Crowdfunding.init');
 
-        $period = (!$this->params->get("period", 7)) ? 7 : $this->params->get("period", 7);
+        $period = (!$this->params->get('period', 7)) ? 7 : $this->params->get('period', 7);
         $options = array(
-            "period" => $period,
-            "description_length" => $this->params->get("description_length", 250),
+            'period' => $period,
+            'description_length' => $this->params->get('description_length', 250),
         );
 
         $updates = $this->getUpdates($options);
 
-        if (!empty($updates)) {
-
+        if (count($updates) > 0) {
             // Array that will contains update IDs.
             $updatesIds = array();
 
             $this->loadLanguage();
 
             // Send messages.
-            jimport("EmailTemplates.init");
-            $email = new \EmailTemplates\Email();
+            jimport('Emailtemplates.init');
+            $email = new Emailtemplates\Email();
             $email->setDb(JFactory::getDbo());
-            $email->load($this->params->get("email_id"));
+            $email->load($this->params->get('email_id'));
 
             if (!$email->getId()) {
-                throw new RuntimeException(JText::_("PLG_CROWDFUNDINGCRON_UPDATES_NOTIFIER_ERROR_INVALID_EMAIL_TEMPLATE"));
+                throw new RuntimeException(JText::_('PLG_CROWDFUNDINGCRON_UPDATES_NOTIFIER_ERROR_INVALID_EMAIL_TEMPLATE'));
             }
 
-            $app       = JFactory::getApplication("site");
-            $emailMode = $this->params->get("email_mode", "plain");
+            $app       = JFactory::getApplication('site');
+            $emailMode = $this->params->get('email_mode', 'plain');
 
             // Set name and e-mail address of the sender in the mail template.
             if (!$email->getSenderName()) {
-                $email->setSenderName($app->get("fromname"));
+                $email->setSenderName($app->get('fromname'));
             }
             if (!$email->getSenderEmail()) {
-                $email->setSenderEmail($app->get("mailfrom"));
+                $email->setSenderEmail($app->get('mailfrom'));
             }
 
             foreach ($updates as $userId => $user) {
-
                 $content = array();
-                foreach ($user["projects"] as $project) {
-
-                    foreach ($project["updates"] as $update) {
+                foreach ($user['projects'] as $project) {
+                    foreach ($project['updates'] as $update) {
                         // Generate message content.
-                        $content[] = "<h2>" . $update["title"] . "</h2>";
-                        $content[] = $update["description"];
-                        $content[] = "<hr />";
+                        $content[] = '<h2>' . $update['title'] . '</h2>';
+                        $content[] = $update['description'];
+                        $content[] = '<hr />';
 
-                        $updatesIds[] = $update["id"];
+                        $updatesIds[] = $update['id'];
                     }
 
-                    $link      = $this->params->get("domain") . JRoute::_(CrowdfundingHelperRoute::getDetailsRoute($project["slug"], $project["catslug"], "updates"));
-                    $content[] = JText::sprintf("PLG_CROWDFUNDINGCRON_UPDATES_NOTIFIER_CAMPAIGN_S_S", $link, $project["title"]);
+                    $link      = $this->params->get('domain') . JRoute::_(CrowdfundingHelperRoute::getDetailsRoute($project['slug'], $project['catslug'], 'updates'));
+                    $content[] = JText::sprintf('PLG_CROWDFUNDINGCRON_UPDATES_NOTIFIER_CAMPAIGN_S_S', $link, $project['title']);
                 }
 
                 // Parse and send message to users.
-                if (!empty($content)) {
+                if (count($content) > 0) {
                     $data = array(
-                        "RECIPIENT_NAME" => $user["name"],
-                        "CONTENT"        => implode("\n", $content)
+                        'RECIPIENT_NAME' => $user['name'],
+                        'CONTENT'        => implode('\n', $content)
                     );
 
                     $email->parse($data);
 
                     $mailer = JFactory::getMailer();
-                    if (strcmp("html", $emailMode) == 0) { // Send as HTML message
-                        $result = $mailer->sendMail($email->getSenderEmail(), $email->getSenderName(), $user["email"], $email->getSubject(), $email->getBody($emailMode), Prism\Constants::MAIL_MODE_HTML);
+                    if (strcmp('html', $emailMode) === 0) { // Send as HTML message
+                        $result = $mailer->sendMail($email->getSenderEmail(), $email->getSenderName(), $user['email'], $email->getSubject(), $email->getBody($emailMode), Prism\Constants::MAIL_MODE_HTML);
                     } else { // Send as plain text.
-                        $result = $mailer->sendMail($email->getSenderEmail(), $email->getSenderName(), $user["email"], $email->getSubject(), $email->getBody($emailMode), Prism\Constants::MAIL_MODE_PLAIN);
+                        $result = $mailer->sendMail($email->getSenderEmail(), $email->getSenderName(), $user['email'], $email->getSubject(), $email->getBody($emailMode), Prism\Constants::MAIL_MODE_PLAIN);
                     }
 
                     if ($result !== true) {
@@ -109,60 +106,60 @@ class plgCrowdfundingCronUpdatesNotifier extends JPlugin
         }
     }
 
-    protected function getUpdates($options = array())
+    protected function getUpdates(array $options = array())
     {
-        $db = JFactory::getDbo();
-        $query = $db->getQuery(true);
+        $db     = JFactory::getDbo();
+        $query  = $db->getQuery(true);
 
         // Truncate string.
-        $descriptionLength = abs(Joomla\Utilities\ArrayHelper::getValue($options, "description_length", 0, "int"));
-        $columnDescription =  (!$descriptionLength) ? "a.description" : 'SUBSTRING_INDEX(a.description, " ", '.(int)$descriptionLength.') as description';
+        $descriptionLength = (int)abs(Joomla\Utilities\ArrayHelper::getValue($options, 'description_length', 0, 'int'));
+        $columnDescription = (!$descriptionLength) ? 'a.description' : 'SUBSTRING_INDEX(a.description, " ", '.(int)$descriptionLength.') as description';
 
         $query
             ->select(
-                "a.id, a.title, ".$columnDescription.", a.record_date, a.project_id, " .
-                "b.user_id"
+                'a.id, a.title, '.$columnDescription.', a.record_date, a.project_id, ' .
+                'b.user_id'
             )
-            ->from($db->quoteName("#__crowdf_updates", "a"))
-            ->innerJoin($db->quoteName("#__crowdf_followers", "b") . " ON a.project_id = b.project_id")
-            ->where("a.record_date >= DATE_SUB(NOW(), INTERVAL ".$options["period"]." DAY)")
-            ->where("a.state = " .(int)Prism\Constants::NOT_SENT);
+            ->from($db->quoteName('#__crowdf_updates', 'a'))
+            ->innerJoin($db->quoteName('#__crowdf_followers', 'b') . ' ON a.project_id = b.project_id')
+            ->where('a.record_date >= DATE_SUB(NOW(), INTERVAL '.$options['period'].' DAY)')
+            ->where('a.state = ' .(int)Prism\Constants::NOT_SENT);
 
         $db->setQuery($query);
         $updates = (array)$db->loadAssocList();
 
         $results = array();
-        if (!empty($updates)) {
-
+        if (count($updates) > 0) {
             $projectsIds = array();
-            $usersIds = array();
+            $usersIds    = array();
+
             foreach ($updates as $update) {
-                $projectsIds[] = $update["project_id"];
-                $usersIds[] = $update["user_id"];
+                $projectsIds[] = $update['project_id'];
+                $usersIds[]    = $update['user_id'];
             }
 
             $projectsIds = array_unique(array_filter($projectsIds));
-            $usersIds = array_unique(array_filter($usersIds));
+            $usersIds    = array_unique(array_filter($usersIds));
 
             $projects = new Crowdfunding\Projects(JFactory::getDbo());
-            $projects->load(array("ids" => $projectsIds));
+            $projects->load(array('ids' => $projectsIds));
 
             $users = new Crowdfunding\User\Users(JFactory::getDbo());
-            $users->load(array("ids" => $usersIds));
+            $users->load(array('ids' => $usersIds));
 
             foreach ($updates as $update) {
-                $user = $users->getUser($update["user_id"]);
+                $user = $users->getUser($update['user_id']);
 
-                $results[$update["user_id"]]["name"]  = (!is_null($user)) ? $user->getName() : "";
-                $results[$update["user_id"]]["email"] = (!is_null($user)) ? $user->getEmail() : "";
+                $results[$update['user_id']]['name']  = ($user !== null) ? $user->getName() : '';
+                $results[$update['user_id']]['email'] = ($user !== null) ? $user->getEmail() : '';
 
-                $project = $projects->getProject($update["project_id"]);
+                $project = $projects->getProject($update['project_id']);
 
-                $results[$update["user_id"]]["projects"][$update["project_id"]]["title"] = (!is_null($project)) ? $project->getTitle() : "";
-                $results[$update["user_id"]]["projects"][$update["project_id"]]["slug"] = (!is_null($project)) ? $project->getSlug() : "";
-                $results[$update["user_id"]]["projects"][$update["project_id"]]["catslug"] = (!is_null($project)) ? $project->getCatSlug() : "";
+                $results[$update['user_id']]['projects'][$update['project_id']]['title'] = ($project !== null) ? $project->getTitle() : '';
+                $results[$update['user_id']]['projects'][$update['project_id']]['slug'] = ($project !== null) ? $project->getSlug() : '';
+                $results[$update['user_id']]['projects'][$update['project_id']]['catslug'] = ($project !== null) ? $project->getCatSlug() : '';
 
-                $results[$update["user_id"]]["projects"][$update["project_id"]]["updates"][] = $update;
+                $results[$update['user_id']]['projects'][$update['project_id']]['updates'][] = $update;
             }
         }
 
